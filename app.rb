fa1f036d83a355dd2422c928b23880aca615ce9d
Yehymmy <you@you.com>
2016-03-04 17:55:38 +0000

    Dashboard! :)

--- app.rb
+++ app.rb
@@ -1,18 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-get '/' do
-  erb :index
-end
+  
+  get '/' do
+    erb :index
+  end
 
-erb:index
-
-end
-
-post '/' do
-  @user = {
-    name "#(params["fname"]) #(params["lname"])",
-    }
-  profile-pic-url "#(params[picurl])"
-erb :dashboard
+  post '/' do
+    @user = {
+      name: "#{params["fname"]} #{params["lname"]}",
+      birthday: "#{params["bday"]}",
+      email: "#{params["email"]}",
+      picture_url: "#{params["pic"]}"
+      }
+    erb :dashboard
+  end
+  
+  
+  
 end
